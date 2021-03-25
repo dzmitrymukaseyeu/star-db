764e5c4383a1ca6fdb7a40d47f4208579f69e2be
Dzmitry Mukaseyeu <dimamukas@gmail.com>
2021-03-25 19:02:48 +0300

    feat: add spinner

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return (
+    <div class="loadingio-spinner-double-ring-0yiqwitycqsd">
+      <div class="ldio-7h9avkc8wpu">
+        <div></div>
+        <div></div>
+        <div><div></div></div>
+        <div><div></div></div>
+      </div>
+    </div>
+  )
+};
+
+export default Spinner;
